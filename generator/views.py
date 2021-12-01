e3dc6db2bf12b97298aa7e8785ceb0e6b9fece33
Rodrigo Poblete <rodrigopoblete.dev@gmail.com>
2021-12-01 00:39:28 -0300

    Initial commit

---
+++ generator/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import random
+
+def home(request):
+    return render(request, 'generator/home.html')
+
+def about(request):
+    return render(request, 'generator/about.html')
+
+def password(request):
+
+    characters = list('abcdefghijklmnopqrstuvwxyz')
+    generated_password = ''
+
+    length = int(request.GET.get('length'))
+
+    if request.GET.get('uppercase'):
+        characters.extend(list('ABCDEFGHIJKLMNOPQRSTUVWXYZ'))
+    if request.GET.get('special'):
+        characters.extend(list('!@#$%^&*()'))
+    if request.GET.get('numbers'):
+        characters.extend(list('1234567890'))
+
+    for character in range(length):
+        generated_password += random.choice(characters)
+
+    return render(request, 'generator/password.html', {'password': generated_password})
